b42739f9110e06ebd3d15fd08348dfedcfbfc5b2
songjoonsuk <52555877+songjoonsuk@users.noreply.github.com>
2019-12-17 14:24:21 +0900

    Add files via upload

--- binomial/ver2/sketch.js
+++ binomial/ver2/sketch.js
@@ -18,12 +18,17 @@ let BallNo = 1;
 let StoryNo = 13;
 let upper_margin;
 let text_color;
-let keyflag = 1;
+let keyflag = 0;
+let ret = 1; 
 
 let left_x1,left_y1,left_x2,left_y2;
 let right_x1,right_y1,right_x2,right_y2;
 
 let img;
+let swing = 5;
+let rand1;
+
+let pre_count = [];
 
 
 function preload() {
@@ -35,15 +40,18 @@ function preload() {
 
 function setup() {
   createCanvas(1900, 1050);
+//  createCanvas(1000, 600);
+
  
   // textFont(font);
   textFont(hanfont);
   textSize(fontsize);
   textAlign(CENTER, CENTER);
+  text_color = color( 255,255,255 );  
+//  text_color = color( 14,141,207 );  
 
-  text_color = color( 14,141,207 );  
 
-  Xcenter = width/3;
+  Xcenter = width*5/12;
   upper_margin = ( height - StoryNo*(diameter + Ymargin) ) / 2 - (2*Ymargin) ;
 
  // Create objects
@@ -58,33 +66,83 @@ function setup() {
       objs.push(new Property());
     }
   }
-
-
+  for( let i =0 ; i< StoryNo;i++) {
+    pre_count[i] = 0; 
+  }
   
+  defaultScreen();
+  msg();
+
 }
 
+
+let xt,yt;
+let id = 0;
+let repeat = 0;
 function draw() {
+  if (mouseIsPressed) keyflag = 1;
+  
   
   if( keyflag ) {
+    if( id == 0 && repeat == 0) define_T();
+ 
+      if( objs[id].color == cir_intense )  {
+          defaultScreen();
+          fill ( 255 ) ;
    
-    noStroke();
-    keyflag = 0;
+   //      rand1 = random( -swing , swing );
+   //       ellipse(objs[id].Xcoor+rand1, objs[id].Ycoor, objs[id].diameter );
+          ellipse(objs[id].Xcoor, objs[id].Ycoor, objs[id].diameter );
+         
+       
+      }  // if
+
+
+    
+      repeat = repeat + 1 ;
+      if( repeat == 1 ) {
+        repeat = 0;
+        id = id + 1;
+        if( id == objs.length ) {
+          id = 0;keyflag = 0;
+          for( let i =0 ; i< StoryNo;i++) {
+            pre_count[i] = objs[i+arr[StoryNo-1]].count;
+          }
+          defaultScreen();
+        }
+      }
+
+  } // keyflag
+}
+
+function defaultScreen() {
+//  background(114, 98, 95);
+  background(50, 89, 100);
+
+  image(img, width/40, height/20 , width/10 , height/10 );
  
-    background(114, 98, 95);
-    image(img, width/40, height/20 , width/10 , height/10 );
- 
-    title();
-    define_T();
-    init_layout();   
-    draw_boundary() ;
-  
-  }
+  title();
+   
+  draw_hurdle(); 
+    
+   
+  display_count();
+  draw_boundary();
+
+
 }
 
 function title() {
   textAlign(CENTER);
   fill( 230, 150, 47 );
   text('이항분포기      ', Xcenter,  (upper_margin*3/4) );
+}
+
+function msg() {
+  textAlign(RIGHT);
+  fill( 255 );
+  text('[마우스 클릭] 또는 [키보드]    ', width,  (upper_margin*3/4) );
+
 }
 
 function binomial() {
@@ -114,7 +172,6 @@ function define_T() {
   let pre_t;
   h = 0;
  
-
   for (let i = 0; i < objs.length; i++)  objs[i].desel();
 
   objs[0].sel();
@@ -132,14 +189,19 @@ function define_T() {
     }
   }
   
+}
 
+function display_count() {
+
+  let count;
 
   fill( text_color );
   let Xref = Xcenter- StoryNo*(diameter+Xmargin)/2 ; 
   let Yref = upper_margin + StoryNo * ( diameter + Ymargin )+70; 
-  for( i =0 ; i< StoryNo;i++) {
-    text( objs[i+arr[StoryNo-1]].count , Xref+i*(diameter+Xmargin) , Yref );
-   }
+  for( let i =0 ; i< StoryNo;i++) {
+    text( pre_count[i] , Xref+i*(diameter+Xmargin) , Yref );
+//    text( objs[i+arr[StoryNo-1]].count , Xref+i*(diameter+Xmargin) , Yref + 50 );
+  }
 }
 
 function keyPressed() {
@@ -149,25 +211,30 @@ function keyPressed() {
   }
 }
 
-function init_layout() {
+function display_ball( i ) {
 
-  for (let i = 0; i < objs.length; i++) {
+   
+  if( objs[i].color == cir_intense ) {	
 
-    if( objs[i].color == cir_intense ) {	
-      fill ( objs[i].color ) ;
-      ellipse(objs[i].Xcoor, objs[i].Ycoor, objs[i].diameter );
-    }
+    console.log(i);
+
+    fill ( cir_intense ) ;
+    ellipse(objs[i].Xcoor, objs[i].Ycoor, objs[i].diameter );
+
+    ret = 1;   
   }
+  else ret = 0;
   
+}
+
+function draw_hurdle() {
   for (let i = 0; i < (objs.length - StoryNo) ; i++) {
     fill ( objs[i].j_color ) ;
 //    ellipse(objs[i].jx, objs[i].jy, 15 );
 
-      triangle( objs[i].jx, objs[i].jy , objs[i].j2x ,objs[i].j2y ,objs[i].j3x ,objs[i].j3y );
-
+    triangle( objs[i].jx, objs[i].jy , objs[i].j2x ,objs[i].j2y ,objs[i].j3x ,objs[i].j3y );
+    
   }
-
-
 }
 
 function draw_boundary() {
@@ -190,7 +257,7 @@ function draw_boundary() {
   stroke(0);
   line( left_x1,left_y1 , left_x2,left_y2);
   line( right_x1,right_y1 , right_x2,right_y2);
-
+  noStroke();
 
   
 }
@@ -205,6 +272,7 @@ class Property {
     this.color = circle_color[0];
     this.TF = 0;
     this.count = 0;
+    
     this.jx = this.Xcoor ;
     this.jy = this.Ycoor + diameter/2 + 5  ;
     this.j2x = this.jx - ( Xmargin/4 ) ;
@@ -219,6 +287,7 @@ class Property {
     this.color = circle_color[1];
     this.TF = 1;
     this.count += 1;
+    
   }
 
   desel() {
